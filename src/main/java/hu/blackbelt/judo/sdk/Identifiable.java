6ec50e9262fdb43bd3009b90f412f45868678639
gaborflorian <114913193+gaborflorian@users.noreply.github.com>
2024-04-05 15:56:58 +0200

    JNG-5627 Replace the structured-map-proxy (#69)
    * remove structured-map-proxy dependency
    
    * add mapholder interface and add back the asMap functions
    
    * JNG-5627 [Release] Updating versions
    
    * add the compositeclassLoader
    
    * JNG-5627 [Release] Updating versions
    
    * remove map-proxy
    
    * remove classloader

--- src/main/java/hu/blackbelt/judo/sdk/Identifiable.java
+++ src/main/java/hu/blackbelt/judo/sdk/Identifiable.java
@@ -1,20 +1,13 @@
 package hu.blackbelt.judo.sdk;
 
-import hu.blackbelt.structured.map.proxy.annotation.Key;
-
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 public interface Identifiable extends Serializable {
 
-    @Key(name = "__identifier")
     Serializable getIdentifier();
 
-    @Key(name = "__entityType")
     String getEntityType();
 
-    @Key(name = "__version")
     Integer getVersion();
 
     <T> T adaptTo(Class<T> target);
